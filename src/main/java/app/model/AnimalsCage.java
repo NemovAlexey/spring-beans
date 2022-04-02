e73c431cc7888f74b38d5a62c570ee1fc64beefc
Nemov Aleksei <nemov90@mail.ru>
2022-04-02 22:46:58 +0300

    Task 2.1.3 update

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,18 +1,17 @@
 package app.model;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
+    private final Animal animal;
+    private final Timer timer;
 
-    @Autowired
-    @Qualifier("dog")
-    private Animal animal;
-
-    @Autowired
-    private Timer timer;
+    public AnimalsCage(@Qualifier("dog") Animal animal, Timer timer) {
+        this.animal = animal;
+        this.timer = timer;
+    }
 
     public Timer getTimer() {
         return timer;
